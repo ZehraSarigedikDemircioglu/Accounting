9f2d217466d039012c2102eeb26fc358772fa808
sefika86 <sefikaakin86@gmail.com>
2023-05-03 13:50:51 -0400

    state entity ready

---
+++ src/main/java/com/smart/accounting/State.java
@@ -0,0 +1,14 @@
+package com.smart.accounting;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class State extends BaseEntity{
+
+    private String stateCode;
+    private String stateName;
+}

4ec6724e1acdec9ac57241325221ca769e7fa427
owermaiden <ower.maiden@cybertekschool.com>
2023-05-03 13:26:21 -0400

    address entity ready

---
+++ src/main/java/com/smart/accounting/Address.java
@@ -0,0 +1,23 @@
+package com.smart.accounting;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Data
+@Getter
+@Setter
+//@Entity
+public class Address extends BaseEntity{
+
+    private String description;
+    private String zip_code;
+
+    /*
+    private State state;
+
+     */
+
+}

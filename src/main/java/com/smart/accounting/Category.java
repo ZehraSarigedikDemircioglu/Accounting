d2247f06104e08f12252316ddda693f236e297ac
sopiokvantaliani <skvantaliani@gmail.com>
2023-05-03 13:54:17 -0400

    Category Entity

--- src/main/java/com/smart/accounting/Category.java
+++ src/main/java/com/smart/accounting/Category.java
@@ -1,8 +1,16 @@
 package com.smart.accounting;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table (name = "category")
+@NoArgsConstructor
+@Data
 public class Category extends BaseEntity{
 
     String categoryName;

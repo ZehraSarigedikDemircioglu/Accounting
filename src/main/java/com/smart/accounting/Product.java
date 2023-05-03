2c48a803aa93cd98bf81c8bf93e9be4e3a8fe7d4
Mariya <mariyatelyuk@gmail.com>
2023-05-03 18:03:33 -0400

    Edited Product

--- src/main/java/com/smart/accounting/Product.java
+++ src/main/java/com/smart/accounting/Product.java
@@ -6,14 +6,21 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
+@Table(name = "product")
 public class Product extends BaseEntity {
 
     private String name;
-    private int price;
-    //private Categoty categotyId;
+    private BigDecimal price;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Category category;
 
 }
